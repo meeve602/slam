2a17a0b022331280450333bd5331b4f029a346ef
Vistual <Vistual>
2022-06-30 04:06:28 -0700

    1

---
+++ build/CMakeFiles/6.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/a/slam/6/6.cpp" "/home/a/slam/build/CMakeFiles/6.dir/6.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"x11\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/root/slam/lambook2-master/3rdparty/Pangolin/Pangolin"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_core/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_display/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_opengl/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_image/include"
+  "/usr/local/include/eigen3"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_windowing/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/build/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_vars/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_geometry/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/tinyobj/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_glgeometry/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_packetstream/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_plot/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_scene/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_tools/include"
+  "/home/a/slambook2-master/3rdparty/Pangolin/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
